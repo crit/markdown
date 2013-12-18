a66c3915421d8bf47e8c8a2de82b168a907ca729
Crit Russell <crit.gfg@gmail.com>
2013-12-18 10:32:30 -0700

    updates

--- src/Markdown/Markdown.php
+++ src/Markdown/Markdown.php
@@ -4,13 +4,28 @@ namespace Markdown;
 use \HTML_To_Markdown;
 use Michelf\MarkdownExtra;
 
+/**
+ * Class Markdown
+ *
+ * @package Markdown
+ */
 class Markdown
 {
+    /**
+     * @param string $md
+     *
+     * @return mixed
+     */
     static function ToHTML($md)
     {
         return MarkdownExtra::defaultTransform($md);
     }
 
+    /**
+     * @param string $html
+     *
+     * @return string
+     */
     static function FromHTML($html)
     {
         $markdown = new HTML_To_Markdown();
